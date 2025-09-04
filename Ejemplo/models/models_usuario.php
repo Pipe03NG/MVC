711cda29b8f54b435288823fa60964724a8ba796
Pipe03NG <fnietogalvis@gmail.com>
2025-09-03 21:03:10 -0500

    Actualizacion Ejemplo

---
+++ Ejemplo/models/models_usuario.php
@@ -0,0 +1,49 @@
+<?php
+
+Class Usuario{
+
+    Public $nombre;
+    Public $apellidos;
+    Public $email;
+    Public $password;
+
+    function getNombre(){
+        return $this->nombre;
+    }
+
+    function getapellidos(){
+        return $this->apellidos;
+    }
+
+    function getemail(){
+        return $this->email;
+    }
+
+    function getpassword(){
+        return $this->password;
+    }
+
+
+
+    //setter
+
+    function setnombre($nombre){
+        $this->nombre = $nombre;
+    }
+    
+    function setapellidos($apellidos){
+        $this->apellidos = $apellidos;
+    }
+
+    
+    function setemail($email){
+        $this->email = $email;
+    }
+
+    function setpassword($password){
+        $this->password = $password;
+    }
+
+}
+
+?>
